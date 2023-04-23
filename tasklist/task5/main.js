cddc528427cd9ea14ba7cf82192537ff2b898031
alexandeerdim <128590278+alexandeerdim@users.noreply.github.com>
2023-04-23 09:50:27 +0300

    Add files via upload

---
+++ tasklist/task5/main.js
@@ -0,0 +1,37 @@
+
+
+let products = [
+    { 
+        name: "Маска",
+        price: 3213
+    },
+    {
+        name: "Ласты",
+        price: 1331
+    },
+    {
+        name: "Трубка",
+        price: 3123
+    },
+    {
+        name: "Сумка",
+        price: 4545
+    },
+    {
+        name: "Баллон",
+        price: 8984
+    },
+    {
+        name: "Нож",
+        price: 2433
+    },
+    {
+        name: "Фонарик",
+        price: 1343
+    },
+    {
+        name: "Шлем",
+        price: 5456
+    }
+];
+
